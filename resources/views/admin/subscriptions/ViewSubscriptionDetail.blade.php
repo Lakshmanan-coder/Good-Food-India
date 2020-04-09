c3b64c41f8b71ee36d21c46075f3680350a466d8
Praveenram Balachandran <praveenrambalu@gmail.com>
2020-04-09 14:41:51 +0530

    calender dates ploted

--- resources/views/admin/subscriptions/ViewSubscriptionDetail.blade.php
+++ resources/views/admin/subscriptions/ViewSubscriptionDetail.blade.php
@@ -64,6 +64,12 @@
                                             </tbody>
                                         </table>
                                     </div>
+                                    <div class="col-sm-12">
+                                        <div id="container">
+                                            <!--element which is going to render the Calendar-->
+                                            <div id="element"></div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-sm-6">
@@ -184,4 +190,80 @@
 
 
 
+@endsection
+
+@section('extra_styles')
+     <!--style reference from the Calendar component-->
+     <link href="//cdn.syncfusion.com/ej2/ej2-base/styles/material.css" rel="stylesheet">
+     <link href="//cdn.syncfusion.com/ej2/ej2-buttons/styles/material.css" rel="stylesheet">
+     <link href="//cdn.syncfusion.com/ej2/ej2-calendars/styles/material.css" rel="stylesheet">
+     <script src="https://cdn.syncfusion.com/ej2/dist/ej2.min.js" type="text/javascript"></script>
+     <style>
+         
+
+/* Example - styles */
+
+#container {
+    visibility: hidden;
+    max-width: 250px;
+    margin: 0 auto;
+}
+
+#loader {
+    color: #9dc839;
+}
+
+#element {
+    display: block;
+}
+
+
+     </style>
+@endsection
+
+@section('extra_scripts')
+
+
+<script>
+    var ele = document.getElementById('container');
+    if(ele) {
+        ele.style.visibility = "visible";
+     }   
+</script>
+
+
+@php
+if ($subscribe->dates!="" && $subscribe->dates!=Null) {
+    $results= explode(",",$subscribe->dates);
+    echo "<script>
+        var dates=[";
+
+
+       $count=1;
+    foreach ($results as $result) {
+        $date= explode("-",$result);
+       echo "new Date('$date[1]/$date[0]/$date[2]'),";
+       if ($count==$subscribe->duration) {
+       break;
+       }
+    }
+    echo" ]
+        </script>";
+    
+}
+
+@endphp
+
+<script>
+// var dates=[new Date('1/1/2020'), new Date('1/15/2020'), new Date('1/3/2020'), new Date('1/25/2020')]
+// var dates=[new Date('04-14-2020'),new Date('04-23-2020'),new Date('04-15-2020'),new Date('04-16-2020'),new Date('04-17-2020'),new Date('04-18-2020') ]
+
+
+/*Initialize the calender component*/
+var calendar = new ej.calendars.Calendar({
+        isMultiSelection: true,
+        values: dates
+    });
+    calendar.appendTo('#element');
+</script>
 @endsection
