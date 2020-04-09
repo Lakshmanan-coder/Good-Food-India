518e4a3bb418252bf8513e15878a99d2f4af49c8
Praveenram Balachandran <praveenrambalu@gmail.com>
2020-04-09 16:19:59 +0530

    admin auth

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('/','AdminPagesController@dashboard');
+    Route::get('/','AdminPagesController@dashboard')->middleware('auth');
     // Route::get('/orders', function () {
     //     return view ('admin.orders.viewOrder');
     // });
@@ -29,27 +29,24 @@ Route::prefix('admin')->group(function () {
     // });
 
 
-    Route::get('/add-plan','PlanController@addPlan');
-    Route::post('/add-plan','PlanController@addPlanPost');
-    Route::get('/view-plans', 'PlanController@ViewPlan');
-    Route::get('/view-plan-detail/{id}', 'PlanController@ViewPlanDetail');
+    Route::get('/add-plan','PlanController@addPlan')->middleware('auth');
+    Route::post('/add-plan','PlanController@addPlanPost')->middleware('auth');
+    Route::get('/view-plans', 'PlanController@ViewPlan')->middleware('auth');
+    Route::get('/view-plan-detail/{id}', 'PlanController@ViewPlanDetail')->middleware('auth');
     
     
     
-    Route::get('/users', 'AdminUserController@viewUsers');
-    Route::get('/view-user/{id}', 'AdminUserController@ViewUserDetail');
+    Route::get('/users', 'AdminUserController@viewUsers')->middleware('auth');
+    Route::get('/view-user/{id}', 'AdminUserController@ViewUserDetail')->middleware('auth');
     
     
     
-    Route::get('/calender', function () {
-        return view ('admin.calender.Calender');
-    });
-    Route::get('/calender', 'CalenderController@adminCalender');
+    Route::get('/calender', 'CalenderController@adminCalender')->middleware('auth');
     
     
     
-    Route::get('/subscriptions', 'SubscribeController@viewSubscriptions');
-    Route::get('/view-subscription-detail/{id}', 'SubscribeController@ViewSubscribeDetail');
+    Route::get('/subscriptions', 'SubscribeController@viewSubscriptions')->middleware('auth');
+    Route::get('/view-subscription-detail/{id}', 'SubscribeController@ViewSubscribeDetail')->middleware('auth');
     
     
 
